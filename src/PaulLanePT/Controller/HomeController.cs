c850ecfe3fd22af77b3086f6bd66e53e8050ed57
OliverJACooke <ocooke@hotmail.com>
2017-01-27 12:29:46 +0000

    add mvc setup

---
+++ src/PaulLanePT/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaulLanePT.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
